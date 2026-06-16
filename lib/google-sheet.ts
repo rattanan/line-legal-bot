dd523ad12597ca0a96740766821387bf825d08f9
Rattanan Nantiyakul <rattanan@gmail.com>
2026-06-16 12:27:15 +0700

    fix google private key

--- lib/google-sheet.ts
+++ lib/google-sheet.ts
@@ -2,10 +2,8 @@ import { google } from "googleapis";
 
 const auth = new google.auth.GoogleAuth({
 credentials: {
-client_email:
-process.env.GOOGLE_CLIENT_EMAIL,
-private_key:
-process.env.GOOGLE_PRIVATE_KEY?.replace(
+client_email: process.env.GOOGLE_CLIENT_EMAIL,
+private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(
 /\n/g,
 "\n"
 ),
